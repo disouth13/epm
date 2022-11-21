23611455eef43731dc7393e32473828dc4e6e36e
Apriyadisouth <apriyadisouth13@gmail.com>
2022-11-21 13:22:33 +0700

    epm

--- resources/views/admin/profile/index.blade.php
+++ resources/views/admin/profile/index.blade.php
@@ -3,5 +3,15 @@
 @section('title', 'Profile')
 
 @section('content')
-    
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">@yield('title')</h6>
+        </div>
+        <div class="card-body">
+            <label for="username">Username</label>
+            <input type="username" class="form-control form-control-user" id="username">
+        </div>
+    </div>
+
+
 @endsection
