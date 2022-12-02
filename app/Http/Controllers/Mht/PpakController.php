38df025253008292836f24b9761c9426b3895704
Apriyadisouth <apriyadisouth13@gmail.com>
2022-12-02 16:57:50 +0700

    epm

---
+++ app/Http/Controllers/Mht/PpakController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Mht;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PpakController extends Controller
+{
+    //
+}
--- app/Models/Ppak.php
+++ app/Models/Ppak.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Ppak extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
 }
